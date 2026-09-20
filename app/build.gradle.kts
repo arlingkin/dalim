e336c5e06c58f852da2bdca3b2f7308969c15372
arlingkin <arlingkin@users.noreply.github.com>
2026-09-20 10:09:04 -0400

    fix(M6): enable buildConfig for VERSION_NAME, add missing firewall_off strings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,10 @@ android {
                 else signingConfigs.getByName("release")
         }
     }
+    buildFeatures {
+        buildConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
